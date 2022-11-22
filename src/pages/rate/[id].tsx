892ad32b0998d7633bfd45ab4a1bfcc1b3bfb0ba
Jack Bisceglia <jackbisceglia2000@gmail.com>
2022-11-22 16:06:57 -0500

    sgsdsdgsd

--- src/pages/rate/[id].tsx
+++ src/pages/rate/[id].tsx
@@ -10,11 +10,15 @@ import { trpc } from "../../utils/trpc";
 
 const CDN_URL = "https://res.cloudinary.com/demo/image/fetch/";
 const Star = ({
+  h,
+  w,
   i,
   rating,
   updateRating,
 }: {
   i: number;
+  h: number;
+  w: number;
   rating: number;
   updateRating: (i: number) => void;
 }) => {
@@ -32,8 +36,8 @@ const Star = ({
         onMouseLeave={() => setHover(false)}
         key={i}
         className="mx-[0.25rem] inline p-0"
-        height={18}
-        width={18}
+        height={h}
+        width={w}
       />
     );
   } else {
@@ -43,35 +47,113 @@ const Star = ({
         onMouseEnter={() => setHover(true)}
         key={i}
         className="mx-[0.25rem] inline p-0"
-        height={18}
-        width={18}
+        height={h}
+        width={w}
       />
     );
   }
 };
 
-const RateQuestion = () => {
-  return <h1>test</h1>;
+const RateQuestion = ({
+  question,
+  num,
+  starRtng,
+  updateRating,
+}: {
+  question: string;
+  num: number;
+  starRtng: number;
+  updateRating: (id: number, value: number) => void;
+}) => {
+  const updateStarRating = (i: number) => {
+    updateRating(num, i + 1);
+  };
+  return (
+    <form className="flex w-full flex-col items-center justify-center gap-2 rounded-md border-4 border-marooon-700 bg-gray-300 py-2 px-6">
+      <h1 className="text-2xl font-black text-marooon-700">{question}</h1>
+      <button
+        onClick={(e) => e.preventDefault()}
+        className="rounded-md bg-marooon-700  px-3 pb-[0.125rem] text-white"
+      >
+        {[...new Array(5).fill(0)].map((_, i) => (
+          <Star
+            h={30}
+            w={30}
+            i={i}
+            key={i}
+            rating={starRtng}
+            updateRating={updateStarRating}
+          />
+        ))}
+      </button>
+    </form>
+  );
 };
 
-const YesNo = ({ question }: { question: string }) => {
+const YesNo = ({
+  question,
+  num,
+  value,
+  updateValue,
+}: {
+  question: string;
+  num: number;
+  value: string;
+  updateValue: (id: number, v: string) => void;
+}) => {
   return (
     <form className="flex w-full justify-between gap-2 rounded-md border-4 border-marooon-700 bg-gray-300 py-2 px-6">
       <h1 className="text-2xl font-semibold text-marooon-700">{question}</h1>
       <div className="align-center flex h-full w-min items-center justify-center text-center text-xl text-marooon-700">
-        <label htmlFor="">Yes</label>
-        <input type="radio" className="ml-2 mr-4" />
-        <label htmlFor="">No</label>
-        <input type="radio" className="ml-2 mr-4" />
+        <label htmlFor={`yes-${num}`}>Yes</label>
+        <input
+          checked={value === "yes"}
+          name={`${num}`}
+          id={`yes-${num}`}
+          type="radio"
+          className="ml-2 mr-4"
+          value={value}
+          onClick={() => updateValue(num, "yes")}
+        />
+        <label htmlFor={`no-${num}`}>No</label>
+        <input
+          checked={value === "no"}
+          name={`${num}`}
+          id={`no-${num}`}
+          type="radio"
+          className="ml-2 mr-4"
+          value={value}
+          onClick={() => updateValue(num, "no")}
+        />
       </div>
     </form>
   );
 };
 
 const PageContent = ({ data }: { data: SearchResultType }) => {
+  const [yesNoValues, setYesNoValues] = useState<string[]>([
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const updateValue = (id: number, v: string) => {
+    const vals = [...yesNoValues];
+    vals[id] = v;
+    setYesNoValues(vals);
+  };
+
   const [starRtng, setStarRtng] = useState(0);
   const updateRating = (i: number) => {
     setStarRtng(i + 1);
+  };
+
+  const [formStarRatings, setFormStarRatings] = useState([0, 0, 0, 0, 0]);
+  const updateFormStarRatings = (id: number, value: number) => {
+    const vals = [...formStarRatings];
+    vals[id] = value;
+    setFormStarRatings(vals);
   };
 
   return (
@@ -91,6 +173,8 @@ const PageContent = ({ data }: { data: SearchResultType }) => {
             <button className="rounded-md bg-marooon-700  px-3 pb-[0.125rem] text-white">
               {[...new Array(5).fill(0)].map((_, i) => (
                 <Star
+                  h={18}
+                  w={18}
                   i={i}
                   key={i}
                   rating={starRtng}
@@ -106,15 +190,45 @@ const PageContent = ({ data }: { data: SearchResultType }) => {
           alt={data.title}
           className="h-60"
         />
-        <YesNo question="Am I Pet Friendly?" />
-        <YesNo question="Are Utilities Included?" />
-        <YesNo question="Would You Rent Again?" />
-        <YesNo question="Is Washer/Dryer Included?" />
-        <button className="marooon-700 w-full rounded-md bg-marooon-700 py-2 text-2xl text-white transition-all duration-100 hover:bg-marooon-800">
+        <YesNo
+          updateValue={updateValue}
+          value={yesNoValues[0] ?? ""}
+          num={0}
+          question="Am I Pet Friendly?"
+        />
+        <YesNo
+          updateValue={updateValue}
+          value={yesNoValues[1] ?? ""}
+          num={1}
+          question="Are Utilities Included?"
+        />
+        <YesNo
+          updateValue={updateValue}
+          value={yesNoValues[2] ?? ""}
+          num={2}
+          question="Would You Rent Again?"
+        />
+        <YesNo
+          updateValue={updateValue}
+          value={yesNoValues[3] ?? ""}
+          num={3}
+          question="Is Washer/Dryer Included?"
+        />
+        <button
+          onClick={() => setYesNoValues(["", "", "", ""])}
+          className="marooon-700 w-full rounded-md bg-marooon-700 py-2 text-2xl text-white transition-all duration-100 hover:bg-marooon-800"
+        >
           Submit
         </button>
       </div>
-      <div className="w-1/2">right side</div>
+      <div className="w-1/2">
+        <RateQuestion
+          num={0}
+          starRtng={formStarRatings[0] ?? 0}
+          updateRating={updateFormStarRatings}
+          question="Rate My Price"
+        />
+      </div>
     </>
   );
 };
